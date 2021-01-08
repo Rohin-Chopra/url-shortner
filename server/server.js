ef0b816bc0023cf878553ad7abf37f47142cccce
Rohin1212 <rohinchopra1212@gmail.com>
2021-01-08 15:37:30 +1100

    deployment

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const Url = require("./models/url");
 db.authenticate().then(() => console.log("database connected"));
 db.sync(/* { force: true } */);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(process.env.PORT, () => {
   console.log(`now listening on port ${PORT}`);
