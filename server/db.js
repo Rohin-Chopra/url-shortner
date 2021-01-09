e09a38744997d9ae8bfc11d05760b1e2d4fbb902
Rohin1212 <rohinchopra1212@gmail.com>
2021-01-09 18:40:40 +1100

    will this fix it?

--- server/db.js
+++ server/db.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 module.exports = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
-  port: "5432",
-  host: "ec2-34-200-158-205.compute-1.amazonaws.com",
+  dialectOptions: {
+    ssl: true,
+  },
 });
