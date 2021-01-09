1410d85d5cffba298922da9a977c377bff9ef98e
Rohin1212 <rohinchopra1212@gmail.com>
2021-01-09 18:37:25 +1100

    database connection fix

--- server/db.js
+++ server/db.js
@@ -2,4 +2,7 @@ const Sequelize = require("sequelize");
 
 module.exports = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  protocol: "postgres",
+  port: "5432",
+  host: "ec2-34-200-158-205.compute-1.amazonaws.com",
 });
