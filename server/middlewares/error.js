698ab07f119e319465b60324838dfd63b97bf7ea
Rohin1212 <rohinchopra1212@gmail.com>
2021-01-09 18:44:33 +1100

    improved error handling in prod

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,7 +1,10 @@
 module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "production") {
-    console.log(res.statusCode);
+    if (!res.statusCode) {
+      res.status(500);
+    }
     return res.json({
+      status: `${res.statusCode}`.startsWith("4") ? "error" : "fail",
       message: err.message,
     });
   }
