a3a2d0f75b256cdfabf6f2fbd31e370c34fa5c76
Rohin1212 <rohinchopra1212@gmail.com>
2021-01-11 12:38:03 +1100

    changed folder structure

---
+++ server.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+dotenv.config({});
+
+const app = require("./app");
+const db = require("./db");
+const Url = require("./models/url");
+
+db.authenticate().then(() => console.log("database connected"));
+db.sync();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(process.env.PORT, () => {
+  console.log(`now listening on port ${PORT}`);
+});
