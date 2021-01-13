5cc94a73c15e3b2b4a723030082eacb445c7c635
Rohin1212 <rohinchopra1212@gmail.com>
2021-01-13 16:16:03 +1100

    added script to update database url on dev env

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
+const { exec } = require("child_process");
 const dotenv = require("dotenv");
+
 dotenv.config({});
 
-const app = require("./app");
-const db = require("./db");
-const Url = require("./models/url");
+if (process.env.NODE_ENV === "development") {
+  exec("heroku config:get DATABASE_URL", (err, stdout, stderr) => {
+    if (err) {
+      return;
+    }
+    process.env.DATABASE_URL = stdout.trim();
+    runApp();
+  });
+} else {
+  runApp();
+}
 
-db.authenticate().then(() => console.log("database connected"));
-db.sync();
+const runApp = () => {
+  const app = require("./app");
+  const db = require("./db");
+  const Url = require("./models/url");
 
-const PORT = process.env.PORT || 3000;
+  db.authenticate().then(() => console.log("database connected"));
+  db.sync();
 
-app.listen(process.env.PORT, () => {
-  console.log(`now listening on port ${PORT}`);
-});
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(process.env.PORT, () => {
+    console.log(`now listening on port ${PORT}`);
+  });
+};
