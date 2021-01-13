f87cd3de73c38b137ad7d1c7a0ab8cdd47c546ff
Rohin1212 <rohinchopra1212@gmail.com>
2021-01-13 16:20:09 +1100

    FIX :  fixed error

--- server.js
+++ server.js
@@ -3,18 +3,6 @@ const dotenv = require("dotenv");
 
 dotenv.config({});
 
-if (process.env.NODE_ENV === "development") {
-  exec("heroku config:get DATABASE_URL", (err, stdout, stderr) => {
-    if (err) {
-      return;
-    }
-    process.env.DATABASE_URL = stdout.trim();
-    runApp();
-  });
-} else {
-  runApp();
-}
-
 const runApp = () => {
   const app = require("./app");
   const db = require("./db");
@@ -29,3 +17,15 @@ const runApp = () => {
     console.log(`now listening on port ${PORT}`);
   });
 };
+
+if (process.env.NODE_ENV === "development") {
+  exec("heroku config:get DATABASE_URL", (err, stdout, stderr) => {
+    if (err) {
+      return;
+    }
+    process.env.DATABASE_URL = stdout.trim();
+    runApp();
+  });
+} else {
+  runApp();
+}
